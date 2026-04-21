c5a7b2f4c0a6e26150d1e46d92dfab4b2926ff5b
Ataxanov <asherzod2197@gmail.com>
2026-04-21 20:12:48 +0500

    Add LibraryBook class for managing book borrowing

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+class LibraryBook:
+    def __init__(self, title, is_available=True):
+        self.title = title
+        self.is_available = is_available
 
+    def borrow(self):
+        if self.is_available:
+            self.is_available = False
+            print("Kitob olindi")
+        else:
+            print("Band")
+
+    def return_book(self):
+        self.is_available = True
+        print("Kitob qaytarildi")
+
+
+# Obyekt
+book = LibraryBook("Python")
+book.borrow()
+book.borrow()
+book.return_book()
